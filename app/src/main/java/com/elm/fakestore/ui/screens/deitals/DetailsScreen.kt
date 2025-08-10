ed1eda147158c00afc0b421bcf27873a38436183
Ahmed Maher <ahmaher04@gmail.com>
2025-08-10 19:33:01 +0300

    finish

--- app/src/main/java/com/elm/fakestore/ui/screens/deitals/DetailsScreen.kt
+++ app/src/main/java/com/elm/fakestore/ui/screens/deitals/DetailsScreen.kt
@@ -36,6 +36,8 @@ import kotlinx.coroutines.withContext
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.layout.LazyLayout
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.ui.draw.clip
@@ -106,87 +108,90 @@ fun Ui(product: Products, cartViewModel: CartViewModel?) {
                 .padding(16.dp)
                 .fillMaxSize()
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(24.dp)
-            ) {
-                Image(
-                    painter = rememberAsyncImagePainter(model = product.images.firstOrNull()),
-                    contentDescription = product.title,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(300.dp)
-                        .border(1.dp, Color.White, RoundedCornerShape(16.dp))
-                        .clip(RoundedCornerShape(16.dp)),
-                )
-                Spacer(modifier = Modifier.height(20.dp))
-                Text(
-                    text = "Category: ${product.category.name}",
-                    style = TextStyle(
-                        color = Color.LightGray,
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight.Normal
-                    ),
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-                Text(
-                    product.title,
-                    style = TextStyle(
-                        color = Color.White,
-                        fontSize = 26.sp,
-                        lineHeight = 30.sp,
-                        fontWeight = FontWeight.W600
-                    ),
-                    maxLines = 3
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = "${product.price} EGP",
-                    style = TextStyle(
-                        fontSize = 22.sp,
-                        color = Goldy,
-                        fontWeight = FontWeight.Thin,
-                    ),
-                    modifier = Modifier.padding(bottom = 10.dp)
-                )
-                Text(
-                    product.description,
-                    style = MaterialTheme.typography.bodyMedium,
-                    textAlign = TextAlign.Justify,
-                    modifier = Modifier.padding(bottom = 16.dp)
-                )
+            LazyColumn ( modifier = Modifier
+                .fillMaxSize()
+                .padding(24.dp)) {
 
-                OutlinedButton(
-                    onClick = {
-                        cartViewModel?.addToCart(product)
-                        scope.launch {
-                            snackbarHostState.showSnackbar(
-                                message = "${product.title} added to cart!",
-                                duration = SnackbarDuration.Short
-                            )
-                        }
-                    },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(0xFF323644),
-                        contentColor = Color.White
-                    ),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(48.dp),
-                    enabled = cartViewModel != null
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.cart_ico),
-                        contentDescription = null,
-                        modifier = Modifier.size(18.dp)
-                    )
-                    Spacer(Modifier.width(8.dp))
-                    Text(text = "Add to Cart")
-                }
+
+         item {
+             Image(
+                 painter = rememberAsyncImagePainter(model = product.images.firstOrNull()),
+                 contentDescription = product.title,
+                 contentScale = ContentScale.Crop,
+                 modifier = Modifier
+                     .fillMaxWidth()
+                     .height(300.dp)
+                     .border(1.dp, Color.White, RoundedCornerShape(16.dp))
+                     .clip(RoundedCornerShape(16.dp)),
+             )
+             Spacer(modifier = Modifier.height(20.dp))
+             Text(
+                 text = "Category: ${product.category.name}",
+                 style = TextStyle(
+                     color = Color.LightGray,
+                     fontSize = 16.sp,
+                     fontWeight = FontWeight.Normal
+                 ),
+                 modifier = Modifier.padding(bottom = 8.dp)
+             )
+             Text(
+                 product.title,
+                 style = TextStyle(
+                     color = Color.White,
+                     fontSize = 26.sp,
+                     lineHeight = 30.sp,
+                     fontWeight = FontWeight.W600
+                 ),
+                 maxLines = 3
+             )
+             Spacer(modifier = Modifier.height(8.dp))
+             Text(
+                 text = "${product.price} EGP",
+                 style = TextStyle(
+                     fontSize = 22.sp,
+                     color = Goldy,
+                     fontWeight = FontWeight.Thin,
+                 ),
+                 modifier = Modifier.padding(bottom = 10.dp)
+             )
+             Text(
+                 product.description,
+                 style = MaterialTheme.typography.bodyMedium,
+                 textAlign = TextAlign.Justify,
+                 modifier = Modifier.padding(bottom = 16.dp)
+             )
+
+             OutlinedButton(
+                 onClick = {
+                     cartViewModel?.addToCart(product)
+                     scope.launch {
+                         snackbarHostState.showSnackbar(
+                             message = "${product.title} added to cart!",
+                             duration = SnackbarDuration.Short
+                         )
+                     }
+                 },
+                 colors = ButtonDefaults.buttonColors(
+                     containerColor = Color(0xFF323644),
+                     contentColor = Color.White
+                 ),
+                 modifier = Modifier
+                     .fillMaxWidth()
+                     .height(48.dp),
+                 enabled = cartViewModel != null
+             ) {
+                 Icon(
+                     painter = painterResource(id = R.drawable.cart_ico),
+                     contentDescription = null,
+                     modifier = Modifier.size(18.dp)
+                 )
+                 Spacer(Modifier.width(8.dp))
+                 Text(text = "Add to Cart")
+             }
+         }
             }
-        }
+         }
+
         SnackbarHost(
             hostState = snackbarHostState,
             modifier = Modifier.align(Alignment.BottomCenter)
